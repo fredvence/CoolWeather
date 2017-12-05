b2c6cf590d44c37ba507a1c6126f072fe3ddfbae
陈作校 <chenzuoxiao@chenzuoxiaodeMacBook-Pro.local>
2017-12-05 23:12:37 +0800

    Add the method  to convert gson data into the weather entity type data

--- app/src/main/java/com/example/chenzuoxiao/coolweather/util/Utility.java
+++ app/src/main/java/com/example/chenzuoxiao/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.chenzuoxiao.coolweather.db.City;
 import com.example.chenzuoxiao.coolweather.db.County;
 import com.example.chenzuoxiao.coolweather.db.Province;
+import com.example.chenzuoxiao.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -86,5 +88,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
