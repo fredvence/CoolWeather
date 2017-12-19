49d3cd9c52c08a97a1cdb41d6b1ab401d6234f66
陈作校 <chenzuoxiao@chenzuoxiaodeMacBook-Pro.local>
2017-12-19 22:31:07 +0800

    add picture of bing as background

--- app/src/main/java/com/example/chenzuoxiao/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/chenzuoxiao/coolweather/WeatherActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.chenzuoxiao.coolweather.gson.Forecast;
 import com.example.chenzuoxiao.coolweather.gson.Weather;
 import com.example.chenzuoxiao.coolweather.util.HttpUtil;
@@ -37,6 +39,7 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView comfortText;
     private TextView carWashText;
     private TextView sportText;
+    private ImageView bingPicImg;
 
 
     @Override
@@ -56,9 +59,17 @@ public class WeatherActivity extends AppCompatActivity {
         comfortText = (TextView) findViewById(R.id.comfort_text);
         carWashText = (TextView) findViewById(R.id.car_wash_text);
         sportText = (TextView) findViewById(R.id.sport_text);
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         Log.d(TAG, "onCreate: 完成控件初始化");
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather", null);
+        String bingPic = prefs.getString("bing_pic", null);
+
+        if (bingPic != null){
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        }else {
+            loadBingPic();
+        }
         if (weatherString != null){
             //有缓存时，直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
@@ -71,6 +82,34 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
 
+    }
+
+    /**
+     * 加载bing每日一图
+     */
+    private void loadBingPic() {
+
+        String resquestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(resquestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final  String bingPic  = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
     }
 
     /**
@@ -119,6 +158,7 @@ public class WeatherActivity extends AppCompatActivity {
                         });
                     }
                 });
+                loadBingPic();
     }
 
     public void showWeatherInfo(Weather weather){
